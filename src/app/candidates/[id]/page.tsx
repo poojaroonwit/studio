a818873fb2bfd9c6b02af02cd7420ef97f6fe8a3
24ep <jaroonwit.pool@gmail.com>
2025-07-07 23:21:05 +0700

    debug

--- src/app/candidates/[id]/page.tsx
+++ src/app/candidates/[id]/page.tsx
@@ -558,6 +558,7 @@ export default function CandidateDetailPage() {
   };
 
   const jobMatches =
+    candidate &&
     candidate.parsedData &&
     'job_matches' in candidate.parsedData &&
     candidate.parsedData.job_matches &&

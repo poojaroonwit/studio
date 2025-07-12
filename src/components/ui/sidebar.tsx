42bcc00ebd8f18d289fa481a5cd233db237089a3
24ep <jaroonwit.pool@gmail.com>
2025-07-13 02:17:14 +0700

    debugg

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -350,7 +350,7 @@ const SidebarHeader = React.forwardRef<
     <div
       ref={ref}
       data-sidebar="header"
-      className={cn("flex flex-col gap-2 p-2 pt-6", className)}
+      className={cn("flex flex-col gap-2 p-2 pt-2", className)}
       {...props}
     />
   )

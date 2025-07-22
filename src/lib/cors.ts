1c9ebabbb2b0bae8ac00275aa320084c852ea45b
Jaroonwit Poolnai <jaroonwit.poo@qsncc.com>
2025-07-22 18:11:49 +0700

    debug

--- src/lib/cors.ts
+++ src/lib/cors.ts
@@ -2,8 +2,9 @@ import { NextRequest } from "next/server";
 
 export function handleCors(req: NextRequest) {
   return {
-    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Origin": "http://localhost:3000", // Set to your frontend URL
     "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
     "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Requested-With",
+    "Access-Control-Allow-Credentials": "true",
   };
 } 

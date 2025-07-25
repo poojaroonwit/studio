990a50d0750875a839389e396ccc4cb633b13e25
Jaroonwit Poolnai <jaroonwit.poo@qsncc.com>
2025-07-25 16:02:50 +0700

    debug

--- src/lib/chartjs-setup.ts
+++ src/lib/chartjs-setup.ts
@@ -1,4 +1,4 @@
-import { Chart as ChartJS, LinearScale, PointElement, Tooltip, Legend, TimeScale } from 'chart.js';
+import { Chart as ChartJS, LinearScale, PointElement, Tooltip, Legend, TimeScale, ArcElement } from 'chart.js';
 import 'chartjs-adapter-date-fns';
 
-ChartJS.register(LinearScale, PointElement, Tooltip, Legend, TimeScale); 
+ChartJS.register(LinearScale, PointElement, Tooltip, Legend, TimeScale, ArcElement); 

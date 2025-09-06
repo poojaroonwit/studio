17011a3ffb6286222fde528f379fc1494bbe47ce
24ep <jaroonwit.pool@gmail.com>
2025-09-06 20:49:59 +0700

    debug

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -391,7 +391,7 @@ export const AppLayout = memo(({ children }: AppLayoutProps) => {
       <LayoutContainer 
         layout="flex" 
         direction="row"
-        className="bg-background app-layout-container"
+        className="bg-background"
         data-testid="app-layout"
       >
         <Sidebar collapsible="icon" className="border-r border-border" data-testid="sidebar">

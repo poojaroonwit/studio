bdfb7d55c25a0c55525849bfda939b8c300370cb
NCC Jaroonwit Poolnai <jaroonwit.poo@qsncc.com>
2025-11-11 18:05:32 +0700

    debug

--- next.config.js
+++ next.config.js
@@ -15,6 +15,8 @@ const nextConfig = {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
+    // Explicitly disable linting during builds
+    dirs: [],
   },
   
   // Increase body size limit for large file uploads

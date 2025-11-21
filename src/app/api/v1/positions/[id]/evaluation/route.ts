38ff4190599fc886b116220a21e7d87ac8ab2a79
NCC Jaroonwit Poolnai <jaroonwit.poo@qsncc.com>
2025-11-21 14:11:18 +0700

    debug

--- src/app/api/v1/positions/[id]/evaluation/route.ts
+++ src/app/api/v1/positions/[id]/evaluation/route.ts
@@ -92,13 +92,6 @@ export async function GET(
         },
         include: {
           trait: {
-            select: {
-              id: true,
-              name: true,
-              description: true,
-              shortDescription: true,
-              isActive: true
-            },
             include: {
               group: {
                 select: {

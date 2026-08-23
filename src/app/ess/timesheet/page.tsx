6c7e31d208300018a4160f3508200f861a5e2e5b
Jaroonwit poolnai <jaroonwit.pool@gmail.com>
2026-08-23 22:16:04 +0700

    feat: add employee timesheet route

---
+++ src/app/ess/timesheet/page.tsx
@@ -0,0 +1,5 @@
+import { EmployeeSelfServicePage } from '@/components/hr/EmployeeSelfServicePage';
+
+export default function EssTimesheetPage() {
+  return <EmployeeSelfServicePage view="timesheet" />;
+}

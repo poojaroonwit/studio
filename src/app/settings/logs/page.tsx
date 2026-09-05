03dfd4df67ccf29530d7acc79e1147ea6ad56fb5
Jaroonwit poolnai <jaroonwit.pool@gmail.com>
2026-09-05 20:30:48 +0700

    fix(hrive): make application logs page shrink-safe

--- src/app/settings/logs/page.tsx
+++ src/app/settings/logs/page.tsx
@@ -40,7 +40,7 @@ export default function ApplicationLogsPage() {
   }
 
   return (
-    <div className="h-full flex flex-col p-6">
+    <div className="flex h-full min-h-0 flex-col p-6">
       <ApplicationLogsFiltersPanel
         isLoading={logsPage.isLoading}
         levelFilter={logsPage.levelFilter}
